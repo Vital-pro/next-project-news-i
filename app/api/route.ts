379bfa0882db2abfc8a8b23ba497f55b27e99218
Vital-pro <vitupikov@yandex.ru>
2024-05-05 19:48:35 +0300

    changes experimental1

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import  {get2AllNews}  from '../../services/get2AllNews';
 
 export async function GET(params:Request) {
-  await get2AllNews();
-  // return new Response(JSON.stringify(await get2AllNews()));
+  // await get2AllNews();
+  return new Response(JSON.stringify(await get2AllNews()));
 
 }
 
